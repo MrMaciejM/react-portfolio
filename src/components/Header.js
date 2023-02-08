83f74bacef5df42aaa5f5e9bd68418efd0e1f567
maciej <63735702+MrMaciejM@users.noreply.github.com>
2023-02-08 19:03:42 +0000

    added Bootstrap and Projects.js and css files. Working on carousel.

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import "./Header.css";
+function Header() {
+  return (
+    <header>
+      <nav>
+        <a href="_#">Projects</a>
+        <a href="_#">About</a>
+        <a href="_#">Contact</a>
+      </nav>
+    </header>
+  );
+}
+export default Header;
