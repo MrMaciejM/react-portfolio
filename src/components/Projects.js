a7c95aa96c494fbc4ff29f55660665e4c6ca9874
maciej <63735702+MrMaciejM@users.noreply.github.com>
2023-02-09 22:30:09 +0000

    added technology stacks elements and rewrote css styling for projects and other elelements

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,7 +8,30 @@ function Projects() {
       <h2>Projects</h2>
       <div className="projectsContainer">
         <div className="projectWrapper">
+          {/* Project 1 */}
           <div>
+            <img
+              src={require("../assets/images/daily-planner.png")}
+              alt={"daily planner app"}
+            />
+            <p>
+              Daily scheduler app built to help keep track of tasks and show
+              color-coded current, past and future time.
+            </p>
+            <div className="techDiv">
+              <span className="tech techHtml">HTML5</span>
+              <span className=" tech techCss">CSS3</span>
+              <span className=" tech techJquery">jQuery</span>
+              <span className=" tech techJS">JS</span>
+              <span className=" tech techMoment">Moment.js</span>
+            </div>
+          </div>
+          {/* Project 2 */}
+          <div>
+            <img
+              src={require("../assets/images/weather-app.png")}
+              alt={"weather app"}
+            />
             <p>Sample text</p>
           </div>
         </div>
